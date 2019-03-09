fb1d9f18d2a024abd83aa0e484e448f6672f9bd6
Taylor Lowery <admiral.taylor@gmail.com>
2019-03-09 13:56:15 -0800

    building up flask app

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,12 +1,29 @@
-
 # Importing the flask module
-from flask import Flask
+from flask import Flask, request, jsonify
+# import translation and blinky code
+import ConvertToMorse
+
 # Create a flask object named app
 app = Flask(__name__)
+
 # When someone will enter the IP address of Raspberry Pi in the browser, below code will run.
 @app.route("/")
 def main():
 	return "Howdy!"
+
+# test route
+@app.route("/test")
+def test():
+	ConvertToMorse.blink_morse_message("Howdy")
+	return "Test!"
+
+# Route for sending morse messages to the pi
+@app.route('/morse', methods=['POST'])
+def morse():
+	if request.method == 'POST':
+		message = request.json['message']
+		ConvertToMorse.blink_morse_message(message)
+
 #if code is run from terminal
 if __name__ == "__main__":
 	# Server will listen to port 80 and will report any errors.
